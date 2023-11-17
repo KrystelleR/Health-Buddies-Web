a9276549e2fa9425fee8ec55393c42618d9c6de6
LucaBragattx <lucabragatto12@gmail.com>
2023-11-17 20:34:01 +0200

    LucaPush

--- .vs/HealthBuddies/Models/UserProfileModel.cs
+++ .vs/HealthBuddies/Models/UserProfileModel.cs
@@ -8,25 +8,35 @@ namespace HealthBuddies.Models
     public class UserProfileModel
     {
         // User information
+<<<<<<< Updated upstream
         public string uid { get; set; }
         public string Username { get; set; }
         public string Email { get; set; }
         public int Age { get; set; }
         public string Height { get; set; }
+=======
+        public string aboutMe { get; set; }
+        public int age { get; set; }
+        public string email { get; set; }
+        public string gender { get; set; }
+        public string height { get; set; }
+>>>>>>> Stashed changes
         public Boolean imperial { get; set; }
-        public string ProfileImage { get; set; }
+        public Boolean metric { get; set; }
+        public string profileImage { get; set; }
         public bool setDetails { get; set; }
-       // public string Gender { get; set; }
-        
+        public string uid { get; set; }
+        public int userCurrency { get; set; }
+        public string username { get; set; }
+        public string weight { get; set; }
+        public int userCurrentCalories { get; set; }
+
         // Goals
-        public string DailyCalories { get; set; }
-        public int DailySteps { get; set; }
-        public string GoalWeight { get; set; }
-        public int MoveMinutes { get; set; }
-        public int Sleep { get; set; }
-        public string DailyWaterAmount { get; set; }
-        public string Weight { get; set; }
-        // About me
-        //public string AboutMe { get; set; }
+        public int dailyCalories { get; set; }
+        public int dailySteps { get; set; }
+        public int dailyWaterAmount { get; set; }
+        public string goalWeight { get; set; }
+        public int moveMinutes { get; set; }
+        public int sleep { get; set; }
     }
 }
